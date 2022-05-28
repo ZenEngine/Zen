1690f73574372b4f3f1b72c43abe81ce1fde67f0
kihoku <kihoku1107.net@gmail.com>
2022-05-28 14:26:59 +0900

    doxygen生成を追加。

--- Sources/Engine/Math/Public/Math/Vector3.hpp
+++ Sources/Engine/Math/Public/Math/Vector3.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdint>
 
 namespace zen
 {
@@ -23,6 +24,35 @@ namespace zen
         */
         Vector3f(float x, float y, float z) noexcept;
 
+        Vector3f(const Vector3f& other) noexcept = default;
+        Vector3f& operator=(const Vector3f& other) noexcept = default;
+        Vector3f(Vector3f&& other) noexcept = default;
+        Vector3f& operator=(Vector3f&& other) noexcept = default;
+        ~Vector3f() noexcept = default;
+
+        [[nodiscard]] Vector3f operator-() const noexcept;
+        [[nodiscard]] Vector3f operator+(const Vector3f& v) const noexcept;
+        [[nodiscard]] Vector3f operator-(const Vector3f& v) const noexcept;
+        [[nodiscard]] Vector3f operator*(const Vector3f& v) const noexcept;
+        [[nodiscard]] Vector3f operator*(float scale) const noexcept;
+        [[nodiscard]] Vector3f operator/(const Vector3f& v) const noexcept;
+        [[nodiscard]] Vector3f operator/(float scale) const noexcept;
+
+        [[nodiscard]] bool operator==(const Vector3f& v) const noexcept;
+        [[nodiscard]] bool operator!=(const Vector3f& v) const noexcept;
+        float& operator[](int32_t index) noexcept;
+        float operator[](int32_t index) const noexcept;
+        
+        Vector3f& operator+=(const Vector3f& v) noexcept;
+        Vector3f& operator-=(const Vector3f& v) noexcept;
+        Vector3f& operator*=(const Vector3f& v) noexcept;
+        Vector3f& operator/=(const Vector3f& v) noexcept;
+
+        friend Vector3f operator*(float scale, const Vector3f& v) noexcept;
+
+        void setX(float x) noexcept;
+        void setY(float y) noexcept;
+        void setZ(float z) noexcept;
 
         [[nodiscard]]
         float getX() const noexcept;
@@ -76,6 +106,118 @@ namespace zen
     {
     }
 
+    inline Vector3f Vector3f::operator-() const noexcept
+    {
+        return{ -_x, -_y, -_z };
+    }
+
+    inline Vector3f Vector3f::operator+(const Vector3f& v) const noexcept
+    {
+        return{ _x + v._x, _y + v._y, _z + v._z };
+    }
+
+    inline Vector3f Vector3f::operator-(const Vector3f& v) const noexcept
+    {
+        return{ _x - v._x, _y - v._y, _z - v._z };
+    }
+
+    inline Vector3f Vector3f::operator*(const Vector3f& v) const noexcept
+    {
+        return{ _x * v._x, _y * v._y, _z * v._z };
+    }
+
+    inline Vector3f Vector3f::operator*(const float scale) const noexcept
+    {
+        return{ _x * scale, _y * scale, _z * scale };
+    }
+
+    inline Vector3f Vector3f::operator/(const Vector3f& v) const noexcept
+    {
+        return{ _x / v._x, _y / v._y, _z / v._z };
+    }
+
+    inline Vector3f Vector3f::operator/(float scale) const noexcept
+    {
+        const float inverseScale{ 1.0f / scale };
+        return{ _x * inverseScale, _y * inverseScale, _z * inverseScale };
+    }
+
+    inline bool Vector3f::operator==(const Vector3f& v) const noexcept
+    {
+        return (_x == v._x && _y == v._y && _z == v._z);  // NOLINT(clang-diagnostic-float-equal)
+    }
+
+    inline bool Vector3f::operator!=(const Vector3f& v) const noexcept
+    {
+        return (_x != v._x || _y != v._y || _z != v._z);  // NOLINT(clang-diagnostic-float-equal)
+    }
+
+    inline float& Vector3f::operator[](const int32_t index) noexcept
+    {
+        // ZEN_EXPECTS_MSG(0 <= index && index <= 2, u"IndexOutOfRange");
+        if (index == 0) {
+            return _x;
+        }
+        else if (index == 1) {
+            return _y;
+        }
+        else {
+            return _z;
+        }
+    }
+
+    inline float Vector3f::operator[](const int32_t index) const noexcept
+    {
+        // ZEN_EXPECTS_MSG(0 <= index && index <= 2, u"IndexOutOfRange");
+        if (index == 0) {
+            return _x;
+        }
+        else if (index == 1) {
+            return _y;
+        }
+        else {
+            return _z;
+        }
+    }
+
+    inline Vector3f& Vector3f::operator+=(const Vector3f& v) noexcept
+    {
+        _x += v._x;
+        _y += v._y;
+        _z += v._z;
+        return *this;
+    }
+
+    inline Vector3f& Vector3f::operator-=(const Vector3f& v) noexcept
+    {
+        _x -= v._x;
+        _y -= v._y;
+        _z -= v._z;
+        return *this;
+    }
+
+    inline Vector3f& Vector3f::operator*=(const Vector3f& v) noexcept
+    {
+        _x *= v._x;
+        _y *= v._y;
+        _z *= v._z;
+        return *this;
+    }
+
+    inline Vector3f& Vector3f::operator/=(const Vector3f& v) noexcept
+    {
+        _x /= v._x;
+        _y /= v._y;
+        _z /= v._z;
+        return *this;
+    }
+
+    inline Vector3f operator*(const float scale, const Vector3f& v) noexcept
+    {
+        return{ v * scale };
+    }
+
+
     inline float Vector3f::getX() const noexcept
     {
         return _x;
@@ -89,6 +231,21 @@ namespace zen
     inline float Vector3f::getZ() const noexcept
     {
         return _z;
+    }
+
+    inline void Vector3f::setX(const float x) noexcept
+    {
+        _x = x;
+    }
+
+    inline void Vector3f::setY(const float y) noexcept
+    {
+        _y = y;
+    }
+
+    inline void Vector3f::setZ(const float z) noexcept
+    {
+        _z = z;
     }
 
     inline Vector3f Vector3f::cross(const Vector3f& v1, const Vector3f& v2) noexcept
