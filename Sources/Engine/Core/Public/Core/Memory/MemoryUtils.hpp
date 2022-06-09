bc1d628db3dea37677f41ed09e2739b963469865
kihoku <kihoku1107.net@gmail.com>
2022-06-10 01:56:24 +0900

    [update]shared_ptr実装途中。

--- Sources/Engine/Core/Public/Core/Memory/MemoryUtils.hpp
+++ Sources/Engine/Core/Public/Core/Memory/MemoryUtils.hpp
@@ -1,6 +1,32 @@
 #pragma once
+#include <type_traits>
+#include <memory> // @TODO: Delete pointer_traisを移植し、to_addressのSTL依存を外す。
 
 namespace zen
 {
+    /**
+     * @brief std::addressof
+    */
+    template<typename T>
+    [[nodiscard]] constexpr T* addressof(T& value) noexcept
+    {
+        return __builtin_addressof(value);
+    }
 
+    template <class T>
+    const T* addressof(const T&& value) = delete;
+
+    template<typename T>
+    constexpr T* toAddress(T* ptr) noexcept
+    {
+        static_assert(!std::is_function_v<T>);
+        return ptr;
+    }
+
+    template<typename T>
+    constexpr auto toAddress(const T& ptr) noexcept
+    {
+        // @TODO: ここを独自に変えると、STL(memory)から切り離せる。
+        return std::to_address(ptr);
+    }
 }
--- Sources/Engine/Core/Public/Core/Memory/SharedPtr.hpp
+++ Sources/Engine/Core/Public/Core/Memory/SharedPtr.hpp
@@ -1,14 +1,113 @@
 #pragma once
+#include <type_traits>
+#include <atomic>
 
 namespace zen
 {
-    template<typename T>
+    enum class ThreadPolicy : uint8_t
+    {
+        NotThreadSafe = 0,
+        ThreadSafe
+    };
+
+
+    namespace detail
+    {
+        template<ThreadPolicy> struct RefCounterTypeSelector;
+
+        template<>
+        struct RefCounterTypeSelector<ThreadPolicy::NotThreadSafe>
+        {
+            using type = int32_t;
+        };
+
+        template<>
+        struct RefCounterTypeSelector<ThreadPolicy::ThreadSafe>
+        {
+            using type = std::atomic<int32_t>;
+        };
+
+        template<ThreadPolicy Policy>
+        struct TRefCounter
+        {
+            using counter_type = RefCounterTypeSelector<Policy>::type;
+
+            TRefCounter(const TRefCounter&) = delete;
+            TRefCounter& operator=(const TRefCounter&) = delete;
+
+            void addReference() noexcept
+            {
+
+            }
+
+            void release()
+            {
+
+            }
+
+            [[nodiscard]] int32_t getUseCount() const noexcept
+            {
+                return _numUses;
+            }
+
+        private:
+            counter_type _numUses{ 1 };
+        };
+    }
+
+    template<typename T, ThreadPolicy Policy>
     class TSharedPtr
     {
     public:
-        using element_type = T;
+        using element_type = std::remove_extent_t<T>;
+
+        constexpr TSharedPtr() noexcept
+            : _ptr{ nullptr }
+            , _refCounter{ nullptr }
+        {
+        }
+
+        ~TSharedPtr() noexcept
+        {
+            if (_refCounter)
+            {
+                _refCounter->release();
+            }
+        }
+
+        [[nodiscard]] std::add_lvalue_reference_t<T> operator*() const noexcept
+        {
+            return *_ptr;
+        }
+
+        [[nodiscard]] element_type* operator->() const noexcept
+        {
+            ZEN_ASSERT_SLOW(isValid());
+            return _ptr;
+        }
+
+        [[nodiscard]] explicit operator bool() const noexcept
+        {
+            return _ptr != nullptr;
+        }
+
+        [[nodiscard]] bool isValid() const noexcept
+        {
+            return _ptr != nullptr;
+        }
+
+        [[nodiscard]] element_type* get() const noexcept
+        {
+            return _ptr;
+        }
+
+        [[nodiscard]] int32_t getUseCount() const noexcept
+        {
+            return _refCounter ? _refCounter->getUseCount() : 0;
+        }
 
     private:
-
+        element_type* _ptr;
+        detail::TRefCounter<Policy>* _refCounter;
     };
 }
